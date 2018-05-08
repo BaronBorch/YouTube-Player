3a62d3c4aaa9e77038476af084f8cb6420c4c581
BaronBorch <38687950+BaronBorch@users.noreply.github.com>
2018-05-08 12:15:43 +0200

    Add files via upload

--- volumebar/main/gui/gui.c
+++ volumebar/main/gui/gui.c
@@ -4,39 +4,43 @@
 #include <string.h>
 #include "gui.h"
 
-bool gui(double height, char inscription[5]);
+gpointer w;
+
+bool gui(int height, char inscription[5]);
 gboolean draw_rect(GtkWidget *widget, cairo_t *new_cr);
 gboolean draw_rect2(GtkWidget *widget, cairo_t *new_cr);
 gboolean cback(gpointer u);
+gboolean cback2(gpointer w);
 
-double rect_height;
+int rect_height;
 char value[5];
 
 
-bool gui(double height, char inscription[5])
+bool gui(int height, char inscription[5])
 {
     rect_height = height*2;
     strncpy(value, inscription, 5);
 
     gtk_init(0, 0);
 
-    GtkWidget *text_view;
     GtkWidget *window = gtk_window_new(GTK_WINDOW_POPUP);
     gtk_window_move(GTK_WINDOW(window), 1280, 548);
     gtk_window_set_default_size(GTK_WINDOW(window), 60, 200);
     gtk_widget_set_app_paintable(window, TRUE);
+    //gpointer w = &window;
 
     g_signal_connect(G_OBJECT(window), "draw", G_CALLBACK(draw_rect), NULL);
     g_signal_connect(G_OBJECT(window), "draw", G_CALLBACK(draw_rect2), NULL);
-
+    //g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(cback2), NULL);
     gtk_widget_show_all(window);
 
-    g_timeout_add(10, cback, NULL);
+    g_timeout_add(20, cback, NULL);
+    //g_timeout_add(10, cback2, NULL);
+
+    //gtk_widget_hide(window);    // dziala, ale musi być przed gtk_main.
+    //gtk_window_close(GTK_WINDOW(window));     //dziala, ale musi być przed gtk_main.
 
     gtk_main();
-
-    //gtk_window_close(GTK_WINDOW(window));  //nie zamyka okna
-    //gtk_widget_hide(window);    // nie chowa okna
 
     return true;
 }
@@ -54,19 +58,32 @@ gboolean draw_rect(GtkWidget *widget, cairo_t *cr)
 
 gboolean draw_rect2 (GtkWidget *widget, cairo_t *cr)
 {
-    double gap, max_rect_height = 200;
+    int gap, max_rect_height = 200;
 
     gap = max_rect_height - rect_height;
 
     GdkRGBA color;
     cairo_rectangle(cr, 0, gap, 60, rect_height);
     gtk_style_context_get_color (gtk_widget_get_style_context (widget), 0, &color);
-    cairo_set_source_rgba (cr, 0.1, 0.6, 0.1, 0.6);
+    cairo_set_source_rgba (cr, 0.1, 0.6, 0.1, 1.0);
     cairo_fill (cr);
 
     cairo_select_font_face (cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
-    cairo_set_font_size (cr, 30.0);
-    cairo_move_to (cr, 8.0, 110.0);
+    cairo_set_font_size (cr, 29.0);
+
+    if(rect_height > 199)
+    {
+        cairo_move_to (cr, -1.0, 110.0);
+    }
+    else if(rect_height < 20)
+    {
+        cairo_move_to (cr, 20.0, 110.0);
+    }
+    else
+    {
+        cairo_move_to (cr, 9.0, 110.0);
+    }
+
     cairo_text_path (cr, value);
     cairo_set_source_rgb (cr, 1, 0.3, 0);
     cairo_fill_preserve (cr);
@@ -76,10 +93,21 @@ gboolean draw_rect2 (GtkWidget *widget, cairo_t *cr)
 
 gboolean cback(gpointer u)
 {
-    //gtk_widget_hide(u);     //Gtk-CRITICAL **: gtk_widget_hide: assertion 'GTK_IS_WIDGET (widget)' failed
-    //gtk_window_close(u);    //Naruszenie ochrony pami�ci
     gtk_main_quit();
     return FALSE;
 }
+
+gboolean cback2(gpointer w)
+{
+
+    //g_usleep (2000000);
+    //gtk_widget_destroy(widget);
+    //gtk_widget_hide(widget);
+    //gtk_window_close(GTK_WINDOW(w));
+    cairo_destroy(w);
+
+    return TRUE;
+}
+
 
 
--- volumebar/main/gui/gui.h
+++ volumebar/main/gui/gui.h
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool gui(double height, char inscription[5]);
+bool gui(int height, char inscription[5]);
 gboolean draw_rect(GtkWidget *widget, cairo_t *new_cr);
 gboolean draw_rect2(GtkWidget *widget, cairo_t *new_cr);
 
