b3aad1a9d46fa5c9d40aff5f89561b2e42448e17
BaronBorch <38687950+BaronBorch@users.noreply.github.com>
2018-05-10 11:08:00 +0200

    Add files via upload

--- volumebar/main/app/app.c
+++ volumebar/main/app/app.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include "input1.h"
+#include <stdbool.h>
+#include "input.h"
 #include "gui.h"
 #include "app.h"
+
+int c;
+char buf[5];
 
 void app()
 {
     FILE *fp;
-	double a, b, d;\
-	int c;
-	char buf[5];
-
-    pthread_t thread_id;
+    double a, b, d;
 
 
     while(1)
     {
-    if(input_read() == true)
-    {
-	fp = popen("amixer get PCM | awk '$0~/%/{print $5}' | tr -d '[dB]'", "r" );
-	d = fscanf(fp, "%lf", &a);
-	b = (a+102.4)*0.94;
-	c = (int)b;
+        if(input_read() == true)
+        {
+            fp = popen("amixer get PCM | awk '$0~/%/{print $5}' | tr -d '[dB]'", "r" );
+            d = fscanf(fp, "%lf", &a);
+            b = (a+102.4)*0.94;
+            c = (int)b;
 
-    if(c < 0)
-    {
-        c = 0;
-    }
-    snprintf(buf, 5, "%d", c);
-    printf( "%s\n", buf); // tymczasowy sprawdzian czy dzia³a jak powinno.
+            if(c < 0)
+            {
+                c = 0;
+            }
+            snprintf(buf, 5, "%d", c);
+            printf( "%s\n", buf); // tymczasowy sprawdzian czy dzia³a jak powinno.
 
-    gui(c, buf);
-    }
+            gui_start(c, buf);
+        }
     }
 }
--- volumebar/main/app/app.h
+++ volumebar/main/app/app.h
@@ -1,10 +1,6 @@
 #ifndef app_H
 #define app_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 void app();
 
 #endif
