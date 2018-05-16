92a0c0865d336e44a1fb9a0cd08436c4467268be
BaronBorch <38687950+BaronBorch@users.noreply.github.com>
2018-05-16 22:50:20 +0200

    Add files via upload

--- volumebar/main/gui/gui.c
+++ volumebar/main/gui/gui.c
@@ -27,6 +27,7 @@ gboolean draw_rect2 (GtkWidget *widget, cairo_t *cr)
     gap = max_rect_height - rect_height;
     GdkRGBA color;
 
+
     cairo_rectangle(cr, 0, gap, 60, rect_height);
     gtk_style_context_get_color (gtk_widget_get_style_context (widget), 0, &color);
     cairo_set_source_rgba (cr, 0.1, 0.6, 0.1, 1.0);
@@ -65,36 +66,39 @@ void set_gui_hided(int gui_hided_init)
     gui_hided = gui_hided_init;
 }
 
+void gui_init()
+{
+    gtk_init(0, 0);
+    window = gtk_window_new(GTK_WINDOW_POPUP);
+    gtk_window_move(GTK_WINDOW(window), 1280, 548);
+    gtk_window_set_default_size(GTK_WINDOW(window), 60, 200);
+    gtk_widget_set_app_paintable(window, TRUE);
+}
+
 void gui_hide()
 {
-    if(gui_hided == 1)
+    if(gui_hided == 0)
     {
         gtk_init(0, 0);
         gtk_widget_hide(window);
         g_timeout_add(5, cback, NULL);
         gtk_main();
+        gui_hided = 1;
     }
 }
 
 void gui_show(int height, char inscription[5])
 {
-    if(gui_hided == 0)
-    {
-        rect_height = height*2;
-        strncpy(value, inscription, 5);
+    rect_height = height*2;
+    strncpy(value, inscription, 5);
+    gui_hided = 0;
 
-        gtk_init(0, 0);
-        window = gtk_window_new(GTK_WINDOW_POPUP);
-        gtk_window_move(GTK_WINDOW(window), 1280, 548);
-        gtk_window_set_default_size(GTK_WINDOW(window), 60, 200);
-        gtk_widget_set_app_paintable(window, TRUE);
+    g_signal_connect(G_OBJECT(window), "draw", G_CALLBACK(draw_rect), NULL);
+    g_signal_connect(G_OBJECT(window), "draw", G_CALLBACK(draw_rect2), NULL);
 
-        g_signal_connect(G_OBJECT(window), "draw", G_CALLBACK(draw_rect), NULL);
-        g_signal_connect(G_OBJECT(window), "draw", G_CALLBACK(draw_rect2), NULL);
-
-        gtk_widget_set_visible(window, TRUE);
-        g_timeout_add(10, cback, NULL);
-        gtk_main();
-    }
+    gtk_widget_queue_draw(window);
+    gtk_widget_set_visible(window, TRUE);
+    g_timeout_add(10, cback, NULL);
+    gtk_main();
 }
 
