34b6dad2d0c1b617c19140a4be8c9bc7f5377416
BaronBorch <38687950+BaronBorch@users.noreply.github.com>
2018-05-17 17:54:08 +0200

    Add files via upload

--- volumebar/main/input/input.h
+++ volumebar/main/input/input.h
@@ -1,13 +1,6 @@
 #ifndef input_H
 #define input_H
 
-#include <stdio.h>
-#include <fcntl.h>
-#include <linux/input.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 bool input_read(void);
 
 #endif
