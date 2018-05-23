db6f3e6c3a665b938e6211ebd65ea873793078e3
BaronBorch <38687950+BaronBorch@users.noreply.github.com>
2018-05-23 20:11:04 +0200

    Add files via upload

--- volumebar/main/app/app.c
+++ volumebar/main/app/app.c
@@ -11,8 +11,6 @@
 #include "gui.h"
 #include "app.h"
 
-int converted_volume_val;
-char volume_to_show[5];
 static pthread_mutex_t Mutex;
 pthread_t thread1;
 
@@ -34,13 +32,11 @@ void *wait_thread(void *vargp)
     ts.tv_nsec = tv.tv_usec * 1000 + 1000 * 1000 * (timeInMs % 1000);
     ts.tv_sec += ts.tv_nsec / (1000 * 1000 * 1000);
     ts.tv_nsec %= (1000 * 1000 * 1000);
-
-    printf("%s\n", "wait_thread here");
     int n = pthread_mutex_timedlock(&Mutex, &ts);
 
     if(n == ETIMEDOUT)
     {
-        //gui_hide();
+        gui_hide();
         printf("%s\n", "HIDEEeeeeeeeeeeeeeeeeeeeeeeeeeeEE");
     }
 }
@@ -49,11 +45,14 @@ void handle_volume_change()
 {
     FILE *file_p;
     double read_vol, convert_vol, convert_file;
+    int converted_volume_val;
+    char volume_to_show[5];
 
     file_p = popen("amixer get PCM | awk '$0~/%/{print $5}' | tr -d '[dB]'", "r" );
     convert_file = fscanf(file_p, "%lf", &read_vol);
     convert_vol = (read_vol+102.4)*0.94;
     converted_volume_val = (int)convert_vol;
+    pclose(file_p);
 
     if(converted_volume_val < 0)
     {
@@ -64,24 +63,32 @@ void handle_volume_change()
     printf( "%s\n", volume_to_show); // tymczasowy sprawdzian czy dzia³a jak powinno.
 
     pthread_mutex_unlock(&Mutex);
-    //gui_show_volumebar(converted_volume_val, volume_to_show);
+    pthread_join(thread1, NULL);
+    gui_show_volumebar(converted_volume_val, volume_to_show);
     pthread_create(&thread1, NULL, wait_thread, NULL);
 }
 
 void set_HDMI()
 {
-    system("shutdown -h now");// Wkrótce będzie tu HDMI on/off
+    FILE *file_d;
+    int read_display_state, display_state;
+    file_d = popen("sudo vcgencmd display_power | tr -cd [:digit:]", "r");
+    read_display_state = fscanf(file_d, "%li", &display_state);
+
+    if(display_state == 1)
+    {system("sudo vcgencmd display_power 0");}
+    else
+    {system("sudo vcgencmd display_power 1");}
+    pclose(file_d);
 }
 
 void app()
 {
-    //gui_init();
+    gui_init();
     input_read_start();
 
     register_volume_up_callback(handle_volume_change);
     register_volume_down_callback(handle_volume_change);
     register_volume_mute_callback(handle_volume_change);
     register_power_callback(set_HDMI);
-
-    //pthread_mutex_destroy(&Mutex);
 }
