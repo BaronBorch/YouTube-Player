d7bc1c94b8331de1c898c6ec311090128d4652e1
BaronBorch <38687950+BaronBorch@users.noreply.github.com>
2018-06-04 19:24:25 +0200

    Add files via upload

--- volumebar/main/main.c
+++ volumebar/main/main.c
@@ -9,23 +9,17 @@ pthread_t thread;
 
 void *run_chromium(void *vargp)
 {
-    system("chromium-browser --start-fullscreen youtube.com/tv");
+    system("chromium-browser --kiosk --disable-infobars youtube.com/tv");
 }
 
 int main(int argc, char *argv[])
 {
-    if(strcmp ("test", argv[1]) == 0)
-    {
-        printf("%s\n", "test mode, chromium won't start");
-        app();
-        pthread_join(thread, NULL);
-        return 0;
-    }
-    else
+    if(argc == 1)
     {
         pthread_create(&thread, NULL, run_chromium, NULL);
-        app();
-        pthread_join(thread, NULL);
-        return 0;
     }
+
+    app();
+    pthread_join(thread, NULL);
+    return 0;
 }
