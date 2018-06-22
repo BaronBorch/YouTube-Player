05804136accad337680de3d32b127b0598ef7195
BaronBorch <38687950+BaronBorch@users.noreply.github.com>
2018-06-22 21:35:06 +0200

    nnn

--- app/gui/gui.c
+++ app/gui/gui.c
@@ -5,23 +5,10 @@
 #include <stdbool.h>
 #include "gui.h"
 
-GtkWidget *window, *window1, *window2;
-int rect_height, gui_hided;
+GtkWidget *window, *window1, *window2, *window3, *spin;
+gboolean active;
+int rect_height, gui_hided, window_active;
 char value[5];
-event_cb wps_connect_pressed_cb;
-
-void register_wps_connect_callback(event_cb a)
-{
-    wps_connect_pressed_cb = a;
-}
-
-void call_cb(event_cb a)
-{
-    if(a != NULL)
-    {
-        a();
-    }
-}
 
 gboolean cback(gpointer u)
 {
@@ -124,54 +111,54 @@ void gui_show_volumebar(int height, char inscription[5])
     gtk_main();
 }
 
-gboolean button_clicked(GtkWidget *widget, gpointer data)
-{
-    g_print("%s\n", "OK pressed");
-    gtk_widget_destroy(window1);
-    return TRUE;
-}
-
 void draw_statement(char statement[40])
 {
     GtkWidget *fixed, *button, *label; 
 
+    g_object_get (GTK_SPINNER (spin), "active", &active, NULL);
+    if(active)
+    {
+        gtk_spinner_stop (GTK_SPINNER (spin));
+        if(window_active == 3)
+        {
+            gtk_widget_destroy(window3);
+        }
+    }
+
+    window_active = 1;
     gtk_init(0, 0);
     window1 = gtk_window_new(GTK_WINDOW_POPUP);
     fixed = gtk_fixed_new();
     label = gtk_label_new(NULL);
     button = gtk_button_new_with_label("OK");
 
-    gtk_window_set_default_size(GTK_WINDOW(window1), 300, 100);
-    gtk_window_move(GTK_WINDOW(window1), 530, 334);
+    gtk_window_set_default_size(GTK_WINDOW(window1), 1380, 768);
     gtk_container_add(GTK_CONTAINER(window1), fixed);
     gtk_label_set_markup(GTK_LABEL(label), statement);
-    gtk_fixed_put (GTK_FIXED (fixed), label, 100, 20);
-    gtk_fixed_put (GTK_FIXED (fixed), button, 125, 60);
-    g_signal_connect(G_OBJECT(window1), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(button, "clicked", G_CALLBACK(button_clicked), NULL);
+    gtk_fixed_put (GTK_FIXED (fixed), label, 590, 345);
+    gtk_fixed_put (GTK_FIXED (fixed), button, 645, 385);
 
-    gtk_window_set_focus(GTK_WINDOW(window1), NULL);        //
-    gtk_widget_set_can_default(button, TRUE);               //to powinno zaznaczyć przycisk(chyba)
-    gtk_window_set_default(GTK_WINDOW(window1), button);    //
-    gtk_widget_grab_focus(button);                          //
-
-    g_timeout_add(10, cback, NULL);
+    g_timeout_add(50, cback, NULL);
     gtk_widget_set_visible(window1, TRUE);
     gtk_widget_show_all(window1);
     gtk_main();
-}
-
-gboolean button_clicked2(GtkWidget *widget, gpointer data)
-{
-    g_print("%s\n", "OK pressed");
-    call_cb(wps_connect_pressed_cb);
-    return TRUE;
 }
 
 void draw_st_disconnect(char statement[40])
 {
     GtkWidget *fixed, *button, *label, *label1; 
 
+    g_object_get (GTK_SPINNER (spin), "active", &active, NULL);
+    if(active)
+    {
+        gtk_spinner_stop (GTK_SPINNER (spin));
+        if(window_active == 3)
+        {
+            gtk_widget_destroy(window3);
+        }
+    }
+
+    window_active = 2;
     gtk_init(0, 0);
     window2 = gtk_window_new(GTK_WINDOW_POPUP);
     fixed = gtk_fixed_new();
@@ -179,24 +166,70 @@ void draw_st_disconnect(char statement[40])
     label1 = gtk_label_new(NULL);
     button = gtk_button_new_with_label("OK");
 
-    gtk_window_set_default_size(GTK_WINDOW(window2), 500, 150);
-    gtk_window_move(GTK_WINDOW(window2), 430, 309);
+    gtk_window_set_default_size(GTK_WINDOW(window2), 1380, 768);
     gtk_container_add(GTK_CONTAINER(window2), fixed);
     gtk_label_set_markup(GTK_LABEL(label), statement);
     gtk_label_set_markup(GTK_LABEL(label1), "To connect press WPS button on your router and press OK");
-    gtk_fixed_put (GTK_FIXED (fixed), label, 180, 20);
-    gtk_fixed_put (GTK_FIXED (fixed), label1, 18, 50);
-    gtk_fixed_put (GTK_FIXED (fixed), button, 225, 110);
-    g_signal_connect(G_OBJECT(window2), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(button, "clicked", G_CALLBACK(button_clicked2), NULL);
+    gtk_fixed_put (GTK_FIXED (fixed), label, 600, 295);
+    gtk_fixed_put (GTK_FIXED (fixed), label1, 438, 345);
+    gtk_fixed_put (GTK_FIXED (fixed), button, 645, 385);
 
-    gtk_window_set_focus(GTK_WINDOW(window2), NULL); 
-    gtk_widget_set_can_default(button, TRUE); 
-    gtk_window_set_default(GTK_WINDOW(window2), button);
-    gtk_widget_grab_focus(button);
-
-    g_timeout_add(10, cback, NULL);
+    g_timeout_add(50, cback, NULL);
     gtk_widget_set_visible(window2, TRUE);
     gtk_widget_show_all(window2);
     gtk_main();
+}
+
+void wait_screen(GtkApplication *app, gpointer user_data)
+{
+    if(window_active == 2)
+    {
+        gtk_widget_destroy(window2);
+    }
+    window_active = 3;
+    window3 = gtk_application_window_new (app);
+    spin = gtk_spinner_new();
+
+    gtk_window_set_decorated (GTK_WINDOW (window3), FALSE);
+    gtk_window_fullscreen (GTK_WINDOW (window3));
+    gtk_window_set_default_size(GTK_WINDOW(window3), 1380, 768);
+    gtk_container_set_border_width (GTK_CONTAINER(window3), 30);
+    gtk_container_add(GTK_CONTAINER(window3), spin);
+    gtk_spinner_start (GTK_SPINNER (spin));
+
+    gtk_widget_show_all(window3);
+}
+
+int wait_screen_start()
+{
+  GtkApplication *app;
+  int status;
+ 
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (wait_screen), NULL);
+  status = g_application_run (G_APPLICATION (app), 0, 0);
+  g_object_unref (app);
+ 
+  return status;
+}
+
+void draw_st_destroy()
+{
+    switch(window_active)
+    {
+        gtk_init(0, 0);
+        case 1: if(window1 != NULL)
+        {
+            gtk_widget_destroy(window1);
+        }
+        break;
+
+        case 2: if(window2 != NULL)
+        {
+            gtk_widget_destroy(window2);
+        }
+        break;
+        g_timeout_add(5, cback, NULL);
+        gtk_main();
+    }
 }
